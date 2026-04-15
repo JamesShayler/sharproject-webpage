92ca4bc35e820ff05fa504f7f6115f61ee441610
JamesShayler <james.shayler.au@gmail.com>
2026-04-15 15:13:19 +0800

    Added project section

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,38 @@ function App() {
     <>
       <section id="welcome">
         <img
-          src="src/assets/pfp.webp"
+          src="pfp.webp"
           alt="Profile picture"
           className="profile-pic"
         />
         <h1>Welcome to my website!</h1>
         <p>
-          This is the place where I display my projects, and anything else
-          I find interesting. Feel free to explore and get in touch!
+          This is the place where I display my projects, and anything else I find interesting. Feel free to explore and get in touch!
         </p>
       </section>
+
+      <section id="projects">
+        <h2>My projects</h2>
+        <p>Here are some of the projects I've worked on:</p>
+          <section>
+            <a href="https://github.com/JamesShayler/sharproject-webpage" target="_blank">
+              <h3>This website</h3>
+              <img
+                src="placeholder.png"
+                alt="project img"
+              />
+            </a>
+            <a href="https://github.com/JamesShayler/sharproject-webpage" target="_blank">
+              <h3>This website again</h3>
+              <img
+                src="placeholder.png"
+                alt="project img"
+              />
+            </a>
+          </section>
+      </section>
+
+
       <section id="next-steps">
         <div id="social">
           <svg className="icon" role="presentation" aria-hidden="true">
